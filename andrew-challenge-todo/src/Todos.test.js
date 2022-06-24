82364b3bfe0cb173c8e9421c3c940f0b69a3dcd7
EAS-Harrison <harrison@enterpriseautomation.co.uk>
2022-06-24 10:53:27 +0100

    added triple test

--- andrew-challenge-todo/src/Todos.test.js
+++ andrew-challenge-todo/src/Todos.test.js
@@ -9,10 +9,11 @@ test('Rendering Todos component and the text "test" is presnt', () => {
     expect(screen.getByText(/test/)).toBeInTheDocument();
 });
 
-test('Rendering Todos component and the text "test" is presnt', () => {
-
-    let todos = [{ text: "test", toggleTodo: () => { } }]
-    render(<Todos todos={todos} />)
-    expect(screen.getByText(/test/)).toBeInTheDocument();
+test('Rendering Todos component and the text "test" is present', () => {
+    let toggle = jest.fn()
+    let todos = [{ text: "test" }, { text: "test" }, { text: "bob" }]
+    render(<Todos todos={todos} toggleTodo={toggle} />)
+    expect(screen.getAllByText(/test/)).toHaveLength(2);
+    expect(screen.getByText(/bob/)).toBeInTheDocument();
 });
 
